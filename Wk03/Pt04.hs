7ae2692636fa6f6ba83987f149c91e33c0bda070
Tom <tom@tdpain.net>
2021-01-25 16:15:00 +0000

    Add Wk03x04

---
+++ Wk03/Pt04.hs
@@ -0,0 +1,15 @@
+length' lst =
+    if lst == []
+        then 0
+        else  let x:xs = lst in 1 + length' xs
+
+filter' pred lst = 
+    if lst == []
+        then []
+        else let x:xs = lst in if pred x
+            then x : filter pred xs
+            else filter pred xs
+        
+main = do
+    print (length' [1..10])
+    print (filter' (<5) [3,9,2,12,6,4])
